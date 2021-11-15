12342fe33a37476560afb761d60e8dfff17b4fcf
TheBoringDude <iamcoderx@gmail.com>
2021-11-15 18:40:53 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="text-xl">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  // NOTE: configure this if you have another folder with tsx files or (that uses stylinggs)
+  purge: [
+    "./src/layouts/**/*.tsx",
+    "./src/components/**/*.tsx",
+    "./src/modules/**/*.tsx",
+  ],
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      // theme colors
+
+      // add all default colors
+      ...colors,
+    },
+
+    // NOTE: Add the lines below if you want to add a google font
+    fontFamily: {
+      sans: ['"Barlow"', ...defaultTheme.fontFamily.sans],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
